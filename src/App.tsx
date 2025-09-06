4b8e18083117ec9b24d404cec8ec4ef59de5d293
Raksha V G <vgraksha26@gmail.com>
2025-09-06 17:07:40 +0530

    configure wagmi + vite + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-black">
+      
+      <main className="container mx-auto px-4 py-8">
+        Welcome to Somnia Colosseum
+      </main>
+    </div>
+  );
+}
+
+export default App;
